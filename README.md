e47e8b9bfe99a03bee9ed7a89fe267113b309599
Noe Jupiter <noeonjupiter@icloud.com>
2022-07-29 22:10:00 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CoreDataStruct
+
+A description of this package.
---
+++ Sources/CoreDataStruct/CoreDataStruct.swift
@@ -0,0 +1,6 @@
+public struct CoreDataStruct {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
